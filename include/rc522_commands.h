862227e3be0e402b70c6e12a57985b0c3e674fad
mooujarrar <mohyiddine1997@gmail.com>
2024-01-22 22:12:58 +0100

    maint; add rc522 commands in a header file

---
+++ include/rc522_commands.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+
+// MFRC522 commands
+#define RC522_CMD_IDLE           0x00
+#define RC522_CMD_AUTHENT        0x0E
+#define RC522_CMD_RECEIVE        0x08
+#define RC522_CMD_TRANSMIT       0x04
+#define RC522_CMD_TRANSCEIVE     0x0C
+#define RC522_CMD_AUTHENT_KEY_A  0x60
+#define RC522_CMD_AUTHENT_KEY_B  0x61

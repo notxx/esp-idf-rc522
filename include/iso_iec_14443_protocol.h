1b7d34cafde354af10ef0e5e4baedc7272311ca3
mooujarrar <mohyiddine1997@gmail.com>
2024-01-28 20:21:08 +0100

    maint; update the MIFARE commands list

--- include/iso_iec_14443_protocol.h
+++ include/iso_iec_14443_protocol.h
@@ -1,14 +1,23 @@
 #pragma once
 
 /**
- * @brief Constants defined in the ISO_IEC_14443 Protocol.
+ * @brief Constants defined in the MIFARE/ISO_IEC_14443 Protocol.
  *  This constants will be put in the FIFO to be read by the card itself.
- *  A handshake following the protocol have to be done before exchange of data. 
+ *  A handshake following the protocol have to be done before exchange of data.
+ *  Documentation under: https://www.mouser.com/datasheet/2/302/MF1S503x-89574.pdf
+ *                       9.1 - MIFARE Classic command overview 
  */ 
 
-#define RC522_REQA                0x26
-#define RC522_WUPA                0x52
-#define RC522_ANTICOLLISION_CMD   0x93
-#define RC522_SELECT_CMD          0x93
-#define RC522_WRITE_CMD           0xA0
-#define RC522_CASCADE_TAG         0x20
+#define MIFARE_REQA                0x26
+#define MIFARE_WUPA                0x52
+#define MIFARE_ANTICOLLISION_CL_1  {0x93, 0x20}
+#define MIFARE_SELECT_CL_1         {0x93, 0x70}
+#define MIFARE_HALT                {0x50, 0x00}
+#define MIFARE_AUTH_KEY_A          0x60
+#define MIFARE_AUTH_KEY_B          0x61
+#define MIFARE_READ                0x30
+#define MIFARE_WRITE               0xA0
+#define MIFARE_DECREMENT           0xC0
+#define MIFARE_INCREMENT           0xC1
+#define MIFARE_RESTORE             0xC2
+#define MIFARE TRANSFER            0xB0
